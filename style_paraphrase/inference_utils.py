c8e31a8474e4a1f4898c832b37a111ab6f1f8450
timjones101 <biggafish8@gmail.com>
2021-06-14 23:02:08 +0100

    update infrence utils

--- style_paraphrase/inference_utils.py
+++ style_paraphrase/inference_utils.py
@@ -50,7 +50,7 @@ class GPT2Generator(object):
         args.temperature = 0
         args.top_p = top_p
         args.top_k = 1
-        args.device = torch.cuda.current_device()
+        args.device = ('cpu') #torch.cuda.current_device()
 
     def modify_p(self, top_p):
         self.args.top_p = top_p
